111322656c2be1ec9f9de3cff85d1f0507018b59
xapis-21 <xapisofficial@gmail.com>
2025-07-27 02:59:07 +0300

    fix: update environment variable for Sanity API token
    - Changed the environment variable from SANITY_API_READ_TOKEN to SANITY_READ_TOKEN for improved clarity and consistency in configuration.
    - Updated error handling to reflect the new token variable, ensuring proper validation during initialization.

--- src/sanity/lib/token.ts
+++ src/sanity/lib/token.ts
@@ -1,6 +1,6 @@
 import "server-only";
 
-export const token = process.env.SANITY_API_READ_TOKEN;
+export const token = process.env.SANITY_READ_TOKEN;
 if (!token) {
   throw new Error("Missing SANITY_API_READ_TOKEN");
 }
